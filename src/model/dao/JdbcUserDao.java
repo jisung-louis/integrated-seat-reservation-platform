bdfab17e52aebb8f79c3ee560c1bcccece2bb183
jisung-louis <khkh9357@naver.com>
2026-02-11 16:45:56 +0900

    feat : 로그인 성공시 해당 UserDto 객체 반환으로 수정 (기존에는 로그인 성공 여부 반환)

--- src/model/dao/JdbcUserDao.java
+++ src/model/dao/JdbcUserDao.java
@@ -1,5 +1,7 @@
 package model.dao;
 
+import model.dto.UserDto;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,18 +18,23 @@ public class JdbcUserDao implements UserDao{
     }
 
     @Override
-    public boolean login(String id, String password){
+    public UserDto login(String id, String password){
         try {
             String sql = "select * from user where id = ? and password = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, id);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
-            return rs.next(); // 결과가 있으면 true, 없으면 false
+            if(rs.next()){
+                int user_no = rs.getInt("no");
+                String name = rs.getString("name");
+                boolean isAdmin = rs.getBoolean("isAdmin");
+                return new UserDto(user_no,id,password,name,isAdmin);
+            }
         } catch (SQLException e) {
             System.out.println("[시스템오류] SQL 문법 문제 발생 : " + e);
         }
-        return false;
+        return null;
     }
 
     @Override
--- src/model/dao/UserDao.java
+++ src/model/dao/UserDao.java
@@ -1,6 +1,14 @@
 package model.dao;
 
+import model.dto.UserDto;
+
 public interface UserDao {
-    boolean login(String id, String password);
+    /**
+     *
+     * @param id 사용자 아이디
+     * @param password 사용자 비밀번호
+     * @return 해당 유저의 유저번호(user_no) 반환
+     */
+    UserDto login(String id, String password);
     boolean signup(String id, String password, String name, boolean isAdmin);
 }
