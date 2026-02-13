e3e3ad17c5dd069d2107a0845bdbfbdd7694a4e5
Sungeun0318 <kimsungeun0318@naver.com>
2026-02-13 17:07:15 +0900

    feat : JdbcReservationDao파일에서 조회, 예약, 변경, 취소만 구현완료// 조회 부분에서 관리자 기준은 완성됐지만 사용자 기준은 아직 미완성 fix : ReservationController [1] 조회기능에서 뷰 오류 수정

--- src/model/dto/ReservationDto.java
+++ src/model/dto/ReservationDto.java
@@ -5,14 +5,29 @@ public class ReservationDto {
     private int user_no;
     private String seat_code;
     private String reservedAt;
+    
+    // 조인용 추가 필드
+    private String userName; 
+    private String userId;
 
     public ReservationDto() { }
+
     public ReservationDto(int no, int user_no, String seat_code, String reservedAt) {
         this.no = no;
         this.user_no = user_no;
         this.seat_code = seat_code;
         this.reservedAt = reservedAt;
     }
+
+    public ReservationDto(int no, int user_no, String seat_code, String reservedAt, String userName, String userId) {
+        this.no = no;
+        this.user_no = user_no;
+        this.seat_code = seat_code;
+        this.reservedAt = reservedAt;
+        this.userName = userName;
+        this.userId = userId;
+    }
+
     public int getNo() { return no; }
     public void setNo(int no) { this.no = no; }
     public int getUser_no() { return user_no; }
@@ -22,6 +37,11 @@ public class ReservationDto {
     public String getReservedAt() { return reservedAt; }
     public void setReservedAt(String reservedAt) { this.reservedAt = reservedAt; }
 
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
+
     @Override
     public String toString() {
         return "ReservationDto{" +
@@ -29,6 +49,8 @@ public class ReservationDto {
                 ", user_no=" + user_no +
                 ", seat_code='" + seat_code + '\'' +
                 ", reservedAt='" + reservedAt + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userId='" + userId + '\'' +
                 '}';
     }
 }
