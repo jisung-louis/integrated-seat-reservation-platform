35e6722dfd3074e804d976f916b4d175bd0efc2e
jisung-louis <khkh9357@naver.com>
2026-02-13 10:17:38 +0900

    feat : JDBC Seat DAO 구현체 임시 구현 (컴파일 에러 방지용)

--- src/model/dao/JdbcSeatDao.java
+++ src/model/dao/JdbcSeatDao.java
@@ -1,10 +1,34 @@
 package model.dao;
 
+import model.dto.SeatDto;
+
+import java.util.ArrayList;
+
 public class JdbcSeatDao implements SeatDao {
     // [1] 싱글톤
     private JdbcSeatDao(){}
     private static JdbcSeatDao instance = new JdbcSeatDao();
     public static JdbcSeatDao getInstance() {
         return instance;
+    }
+
+    @Override
+    public boolean addSeats(int store_no, String rowcode, String colnum) {
+        return false;
+    }
+
+    @Override
+    public boolean updateSeatStatus(int store_no, String rowcode, String colnum, int status) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteSeats(int store_no, String rowcode, String colnum) {
+        return false;
+    }
+
+    @Override
+    public ArrayList<SeatDto> getSeats(int store_no) {
+        return null;
     }
 }
