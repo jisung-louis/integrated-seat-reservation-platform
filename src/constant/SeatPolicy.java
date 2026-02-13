a92227f063c6aa2b7fa31255138c85b9df54353a
jisung-louis <khkh9357@naver.com>
2026-02-13 10:18:26 +0900

    fix : SeatPolicy 주석 오기 수정

--- src/constant/SeatPolicy.java
+++ src/constant/SeatPolicy.java
@@ -2,11 +2,11 @@ package constant;
 
 public class SeatPolicy {
     /**
-     * @설명 : 좌석 최대 가로길이
+     * @설명 : 좌석 최대 세로길이
      */
     public static final int MAX_SEAT_ROW_COUNT = 10;
     /**
-     * @설명 : 좌석 최대 세로길이
+     * @설명 : 좌석 최대 가로길이
      */
     public static final int MAX_SEAT_COLUMN_COUNT = 10;
     private SeatPolicy() {}
