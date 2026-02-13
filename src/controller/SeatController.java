46288775516741c5c35e99261f6eece0e39209be
jisung-louis <khkh9357@naver.com>
2026-02-13 14:38:01 +0900

    feat : SeatController 구현 (좌석 활성화/비활성화 토글 메소드, 해당 매장의 모든 좌석 반환 메소드)

--- src/controller/SeatController.java
+++ src/controller/SeatController.java
@@ -1,10 +1,61 @@
 package controller;
 
+import constant.SeatPolicy;
+import model.dao.JdbcSeatDao;
+import model.dao.SeatDao;
+import model.dto.SeatDto;
+
+import java.util.ArrayList;
+
 public class SeatController {
     // [1] 싱글톤
     private SeatController(){}
     private static SeatController instance = new SeatController();
     public static SeatController getInstance() {
         return instance;
+    }
+    SeatDao sd = JdbcSeatDao.getInstance();
+
+    public int toggleSeatStatus(int store_no, String rawSeatCode){
+        // [1] rawSeatCode 유효성 체크
+        // [1-1] "B-4" 꼴로 되어 있는지
+        String[] parts = rawSeatCode.split("-");
+        if(parts.length != 2) { return 3; } // "-"로 나눈 문자열이 2개가 아닐 때
+        String colCode = parts[0];
+        String rowNum = parts[1];
+        if (!colCode.matches("^[A-Z]+$")) { return 3; } // colCode가 영문자(대문자)로 이루어져있지 않으면
+        if (!rowNum.matches("^[0-9]+$")) { return 3; } // rowNum이 숫자로 이루어져있지 않으면
+        // [1-2] rowCode와 colNum이 최대값보다 같거나 작은지
+        int colNumber = convertColCodeToNumber(colCode);
+        int rowNumber = Integer.parseInt(rowNum);
+        if (colNumber > SeatPolicy.MAX_SEAT_COLUMN_COUNT) { return 3; }
+        if (rowNumber > SeatPolicy.MAX_SEAT_ROW_COUNT) { return 3; }
+
+        // [2] rawSeatCode(예:"B-4")를 "매장번호-colCode-rowNum"(예 : "1-B-4") 꼴로 정규화
+        String seatCode = Integer.toString(store_no) + "-" + rawSeatCode;
+
+        // [3] 해당 자리가 추가되어 있으면 제거, 없으면 추가 (토글)
+        boolean isSeatExist = sd.isSeatExist(seatCode);
+        if(isSeatExist){
+            boolean result = sd.deleteSeat(seatCode); // 제거
+            return result ? 2 : 0;
+        }
+        else {
+            boolean result = sd.addSeat(seatCode); // 추가
+            return result ? 1 : 0;
+        }
+    }
+    public ArrayList<SeatDto> getSeats(int store_no){
+        return sd.getSeats(store_no);
+    }
+
+    private int convertColCodeToNumber(String colCode) {
+        int result = 0;
+
+        for (int i = 0; i < colCode.length(); i++) {
+            result = result * 26 + (colCode.charAt(i) - 'A' + 1);
+        }
+
+        return result;
     }
 }
